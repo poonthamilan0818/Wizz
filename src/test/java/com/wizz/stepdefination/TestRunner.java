d8c0987791b266af603bb7e8c1a3eb18c13934f0
syedfarith31 <syedfarith31@gmail.com>
2019-05-10 11:12:57 +0530

    test

--- src/test/java/com/wizz/stepdefination/TestRunner.java
+++ src/test/java/com/wizz/stepdefination/TestRunner.java
@@ -6,7 +6,8 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features/", glue = { "com.wizz.stepdefination" }, plugin = {"json:src/test/java/pack/cucumber.json" }, monochrome = true)
+@CucumberOptions(features = "src/test/resources/features/", glue = { "com.wizz.stepdefination" }, plugin = {
+		"json:src/test/java/pack/cucumber.json" }, monochrome = true, dryRun = false, strict = true)
 public class TestRunner {
 
 }
